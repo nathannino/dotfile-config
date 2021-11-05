81371a11bc0c9178b7c2e25234aa7fa64cde2167
Nathan_Nino <yohanpro.jimdo.com@gmail.com>
2021-11-05 15:35:29 -0400

    Fixed update script... but xmonad will break because tested it by updating and the update is bad

--- .xmonad/get_yay_update.sh
+++ .xmonad/get_yay_update.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/bash
+
+# Make sure that yay will get the right values later
+pacman -Qu > /dev/null
+
+# Get the amount of package updates
 yay -Qu | grep -o "\->" | wc -l | xargs printf
