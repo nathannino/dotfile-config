8fd0b2640fa8fa3a3ce85f8a64ebc704edb7eb05
Nathan_Nino <yohanpro.jimdo.com@gmail.com>
2021-11-06 12:14:42 -0400

    Finally figured out why xmobar was not changing colors

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,6 +4,8 @@ import XMonad.Util.EZConfig -- Kinda for stuff like keybindings... [View docs fo
 import XMonad.Util.Ungrab
 import XMonad.Util.Loggers
 
+import XMonad.Layout.ToggleLayouts
+
 import XMonad.Config.Desktop
 
 import XMonad.Actions.Promote
@@ -23,39 +25,30 @@ import XMonad.Hooks.ManageHelpers
 -- Main function, just call other stuff
 main :: IO ()
 main = xmonad 
-	. ewmhFullscreen 
-	. ewmh 
-	. xmobarProp 
-	. withEasySB (statusBarProp "xmobar ~/.xmonad/xmobarrc" (pure myXmobarPP)) defToggleStrutsKey --  Keybindings at : https://xmonad.org/TUTORIAL.html#changing-what-xmonad-sends-to-xmobar
-	. Hacks.javaHack 
-	$ myConfig -- See end of file
-
+        . ewmhFullscreen 
+        . ewmh 
+        . withEasySB (statusBarProp "xmobar ~/.xmonad/xmobarrc" (pure myXmobarPP)) defToggleStrutsKey --  Keybindings at : https://xmonad.org/TUTORIAL.html#changing-what-xmonad-sends-to-xmobar
+        . Hacks.javaHack 
+        $ myConfig -- See end of file
 
 -- Xmobar pretty printer
 myXmobarPP :: PP
 myXmobarPP = def
-	{ ppSep			= magenta " . "
-	, ppTitleSanitize	= xmobarStrip
-	, ppCurrent		= wrap " " "" . xmobarBorder "Top" "#8be9fd" 2
-	, ppHidden		= white . wrap " " ""
-	, ppHiddenNoWindows	= lowWhite . wrap " " ""
-	, ppUrgent		= red . wrap (yellow "!") (yellow "!")
-	, ppOrder		= \[ws, l, _, wins] -> [ws, l, wins]
-	, ppExtras		= [logTitles formatFocused formatUnfocused]
+	{ ppCurrent = yellow . wrap "[" "]" -- Current workspace in xmobar
+	, ppVisible = magenta -- Visible workspaces that are not the current workspace
+	, ppHidden = lightBlue -- Hidden workspaces
+	--, ppHiddenNoWindows = lowWhite -- Hidden workspaces, but no windows
+	, ppTitle = lightBlue . shorten 60 -- Title of window
+	, ppSep = " | " -- Seperator
+	, ppUrgent = red . wrap "!" "!" -- Urgent window in workspace
 	}
-	where
-		formatFocused	= wrap (white		"[") (white	"]") . magenta 	. ppWindow
-		formatUnfocused	= wrap (lowWhite	"[") (lowWhite	"]") . blue 	. ppWindow
-
-		-- Limit name to sane lenght
-		ppWindow :: String -> String
-		ppWindow = xmobarRaw . (\w -> if null w then "untitled window" else w) -- . shorten 30
-
+	where -- Color definitions
 		blue, lowWhite, magenta, red, white, yellow :: String -> String
 		magenta	 = xmobarColor "#ff79c6" ""
 		blue	 = xmobarColor "#bd93f9" ""
-		white	 = xmobarColor "#f8f8f2" ""
-		yellow	 = xmobarColor "#f1fa8c" ""
+		lightBlue= xmobarColor "#d1feff" ""
+		white	 = xmobarColor "#ffffff" ""
+		yellow	 = xmobarColor "#f9fb07" ""
 		red	 = xmobarColor "#ff5555" ""
 		lowWhite = xmobarColor "#bbbbbb" ""
 
