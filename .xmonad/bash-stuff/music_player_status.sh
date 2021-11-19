4219a8fd18e4f9cfe79242bdc6a8e4dfafc0d93b
Nathan_Nino <yohanpro.jimdo.com@gmail.com>
2021-11-19 16:39:20 -0500

    Made the music icon be a cube when there is nothing to print

--- .xmonad/bash-stuff/music_player_status.sh
+++ .xmonad/bash-stuff/music_player_status.sh
@@ -9,7 +9,7 @@ MPD_CURRENT_PLAYING=${MPD_CURRENT_PLAYING//.mp3/}
 ## Prepare the printing
 if test -z "$MPD_CURRENT_PLAYING"
 then
-	PRINTING_STRING="<action=\`notify-send music 'This will open a simple xmenu thing'\` button=1> <fn=1></fn> </action>"
+	PRINTING_STRING="<action=\`notify-send music 'This will open a simple xmenu thing'\` button=1> <fn=1></fn> </action>"
 else
 	PRINTING_STRING="<action=\`notify-send music 'This will open a simple xmenu thing'\` button=1> <fn=1></fn> $MPD_CURRENT_PLAYING </action> <fn=1></fn> "
 fi
